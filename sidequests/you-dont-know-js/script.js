872cc127ffc8431754a32da270d06e0cdef7e80f
Julian Rios <JulianRios.dev@gmail.com>
2018-08-26 08:30:18 -0400

    completed chapter 1 ydkjs

--- sidequests/you-dont-know-js/script.js
+++ sidequests/you-dont-know-js/script.js
@@ -1,15 +1,80 @@
+// version 2
+const TAX_RATE = 0.07; 
+const PHONE_PRICE = 99.99;
+const ACCESSORIES_PRICE = 10;
+const SPENDING_THRESHOLD = 200;
+// const input = document.getElementById("userinput");
+
+let amount = 0;
+let bank_balance = 0;
+let remainingBalance = 0;
+
+function calculateTax(amount) {
+	return amount * TAX_RATE;
+}
+
+function stringFormat(num) {
+	return "$" + num.toFixed(2);
+}
+
+function shop() {
+	bank_balance = prompt("How much are we spending today?");
+	let purchasedPhone = 0;
+	let purchasedAccess = 0;
+	
+	while(amount < bank_balance && amount < SPENDING_THRESHOLD) {
+			// buy new phone
+			amount = amount + PHONE_PRICE;
+			purchasedPhone++;
+			
+			// buy accessories
+			if (amount < SPENDING_THRESHOLD) {
+				amount = amount + ACCESSORIES_PRICE
+				purchasedAccess++;
+			}
+	}
+		// pay the govt taxes
+		amount = amount + calculateTax(amount);
+		if(amount > bank_balance) {
+			// calculates the amount needed to buy
+			remainingBalance = amount - bank_balance;
+			remainingBalance = stringFormat(remainingBalance);
+			console.log(`You can not afford this purchase ${purchasedPhone} phones with ${purchasedAccess} accessories. You need an additional ${remainingBalance} to buy.`)
+		} else {
+			// store current balance
+			remainingBalance = bank_balance - amount;
+			
+			// format number values to strings
+			amount = stringFormat(amount);
+			remainingBalance = stringFormat(remainingBalance)
+			console.log( `You spent: ${amount} and purchased: ${purchasedPhone} phones with ${purchasedAccess} accessories. Your remaining balance is ${remainingBalance}`);
+		}
+}
+
+shop();
+
+// Adds text input by user 
+// function sendInputAfterKeypress () {
+// 	bank_account = input.value="";
+// 	console.log("bugs!");
+// }
+
+// input.addEventListener("keypress", sendInputAfterKeypress);
+
+
+
 // // version 1
 // const TAX_RATE = 0.07; 
 // const PHONE_PRICE = 99.99;
 // const ACCESSORIES_PRICE = 10;
 // const SPENDING_THRESHOLD = 600;
 
-// var amount = 0;
-// var bank_balance = 1111.22;
+// let amount = 0;
+// let bank_balance = 1111.22;
 
 // function shop() {
-// 	var purchasedMerch = 0;
-// 	var costPerPhone = calculateTax();
+// 	let purchasedMerch = 0;
+// 	let costPerPhone = calculateTax();
 // 	while(bank_balance > SPENDING_THRESHOLD) {
 // 			total = costPerPhone + ACCESSORIES_PRICE;
 // 			bank_balance = bank_balance - total;
@@ -28,57 +93,3 @@
 // }
 
 // shop();
-// version 2
-
-const TAX_RATE = 0.07; 
-const PHONE_PRICE = 99.99;
-const ACCESSORIES_PRICE = 10;
-const SPENDING_THRESHOLD = 700;
-
-var amount = 0;
-var bank_balance = 0;
-var remainingBalance = 0;
-
-function calculateTax(amount) {
-	return amount * TAX_RATE;
-}
-
-function stringFormat(num) {
-	return "$" + num.toFixed(2);
-}
-
-function shop() {
-	bank_balance = prompt("How much are we spending today?");
-	var purchasedPhone = 0;
-	var purchasedAccess = 0;
-	
-	while(amount <  bank_balance && amount < SPENDING_THRESHOLD) {
-			// buy new phone
-			amount = amount + PHONE_PRICE;
-			purchasedPhone++;
-			
-			// buy accessories
-			if (amount < SPENDING_THRESHOLD) {
-				amount = amount + ACCESSORIES_PRICE
-				purchasedAccess++;
-			}
-	}
-		// pay the govt taxes
-		amount = amount + calculateTax(amount);
-		if(amount > bank_balance) {
-			// calculates the amount needed to buy
-			remainingBalance = amount - bank_balance;
-			remainingBalance = stringFormat(remainingBalance);
-			console.log(`You can not afford this purchase. You need an additional ${remainingBalance} to buy.`)
-		} else {
-			// store current balance
-			remainingBalance = bank_balance - amount;
-			
-			// format number values to strings
-			amount = stringFormat(amount);
-			remainingBalance = stringFormat(remainingBalance)
-			console.log( `You spent: ${amount} and purchased: ${purchasedPhone} phones with ${purchasedAccess} accessories. Your remaining balance is ${remainingBalance}`);
-		}
-}
-
-shop();
